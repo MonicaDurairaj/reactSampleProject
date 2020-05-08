7a4e1e258c03658c303daf93dce0b83436e2497e
MonicaDurairaj <babydurairaj95@gmail.com>
2020-05-08 16:06:17 -0500

    Updates

---
+++ precache-manifest.89b12ba76ad1af273ecdecbc6d517094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02dd15a10457f06efbfd78161d652e0",
+    "url": "/MonicaDurairaj/reactSampleProject/index.html"
+  },
+  {
+    "revision": "b0072d2468b3de80b050",
+    "url": "/MonicaDurairaj/reactSampleProject/static/css/main.9b1da959.chunk.css"
+  },
+  {
+    "revision": "d1c12cf317fc6637d144",
+    "url": "/MonicaDurairaj/reactSampleProject/static/js/2.9f7ec2f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonicaDurairaj/reactSampleProject/static/js/2.9f7ec2f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0072d2468b3de80b050",
+    "url": "/MonicaDurairaj/reactSampleProject/static/js/main.16a705bc.chunk.js"
+  },
+  {
+    "revision": "f781ee89107eddbee3bb",
+    "url": "/MonicaDurairaj/reactSampleProject/static/js/runtime-main.88b110d6.js"
+  }
+]);
